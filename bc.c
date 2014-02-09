4f6c9f9867a44e463e1c1e4e5e002d3677c7ca7a
xieyong <sheayone@gmail.com>
2014-02-09 22:21:59 +0800

    first commit

---
+++ bc.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+    printf("
+
+--- BEGIN LICENSE KEY ---
+2N63HxEft2bSOcxhhTnvlHQqshl+hEWNCOa8igUWlr7qBEXpEWIaeOd5t
++maN5OMMdVjGbv4NNqGhnJNqmdnQE
+--- END LICENSE KEY -----
+            
+            \n");
+    return 0;
+}
+
+
